0514a322e9a06901ca0e90b6f3f576f27cf35fcc
henryh28 <henry@henryhsu.net>
2014-12-16 12:24:07 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiceBuild::Application.config.secret_token = '89b85897948115dad31f56c16d44f69692747f9433b6348b8f846ac8adbd029afd1abe6986cc80ee1ecaa87318535354c4b6e33df0e2fdca2399e8d3ba15e3b3'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+DiceBuild::Application.routes.draw do
+  root to: "main#index"
+
+
+end
